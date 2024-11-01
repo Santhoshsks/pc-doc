1abca7d47cca1bffc395c6d41ee31d133a280e76
santhoshsks <santhoshkoh@gmail.com>
2024-11-01 12:39:59 +0530

    feat: chatbot

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.svg';
+import Chatbot from 'react-chatbot-kit'
+import 'react-chatbot-kit/build/main.css'
 import './App.css';
+import config from './bot/config.js';
+import MessageParser from './bot/MessageParser.jsx';
+import ActionProvider from './bot/ActionProvider.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-          Hello
-        
-      </header>
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
     </div>
+    
   );
 }
 
