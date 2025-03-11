5cd402bc41a71ca6caa602d3df6f706cd6a67ec9
santhoshsks <santhoshkoh@gmail.com>
2025-03-11 21:03:16 +0530

    cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import InputDesign from './bot/components/InputDesign'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,5 +9,3 @@ root.render(
     <InputDesign />
   </React.StrictMode>
 );
-
-reportWebVitals();
